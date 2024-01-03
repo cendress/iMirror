3760e2a71b286c29308a9d2a95f1c7213ed93cc6
cendress <95492688+cendress@users.noreply.github.com>
2024-01-03 16:18:10 -0500

    Implement custom tab bar

--- iMirror/CustomTabBarVC.swift
+++ iMirror/CustomTabBarVC.swift
@@ -16,15 +16,12 @@ class CustomTabBarVC: UITabBarController {
   
   private func setupTabs() {
     let journalVC = JournalVC()
-    journalVC.view.backgroundColor = .blue
     journalVC.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "text.book.closed"), tag: 0)
     
     let questionPromptsVC = QuestionPromptsVC()
-    questionPromptsVC.view.backgroundColor = .yellow
     questionPromptsVC.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "plus"), tag: 1)
     
     let settingsVC = SettingsVC()
-    settingsVC.view.backgroundColor = .green
     settingsVC.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "gear"), tag: 2)
     
     viewControllers = [journalVC, questionPromptsVC, settingsVC]
--- iMirror/SettingsVC.swift
+++ iMirror/SettingsVC.swift
@@ -12,7 +12,8 @@ class SettingsVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+      view.backgroundColor = .systemCyan
+      
     }
     
 
