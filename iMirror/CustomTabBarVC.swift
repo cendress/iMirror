1cb74f73f20aa873248abe2dae71420c1371aad7
cendress <95492688+cendress@users.noreply.github.com>
2024-01-03 16:04:03 -0500

    Complete setupTabs() function

--- iMirror/CustomTabBarVC.swift
+++ iMirror/CustomTabBarVC.swift
@@ -11,9 +11,22 @@ class CustomTabBarVC: UITabBarController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    
+    setupTabs()
   }
   
-  
+  private func setupTabs() {
+    let journalVC = UIViewController()
+    journalVC.view.backgroundColor = .blue
+    journalVC.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "text.book.closed"), tag: 0)
+    
+    let questionPromptsVC = UIViewController()
+    questionPromptsVC.view.backgroundColor = .yellow
+    questionPromptsVC.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "plus"), tag: 1)
+    
+    let settingsVC = UIViewController()
+    settingsVC.view.backgroundColor = .green
+    settingsVC.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "gear"), tag: 2)
+    
+    viewControllers = [journalVC, questionPromptsVC, settingsVC]
+  }
 }
