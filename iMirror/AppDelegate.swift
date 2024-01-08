a3dcc3e535ae3d46ebd5d9d3eeb9c8a18b4655b6
cendress <95492688+cendress@users.noreply.github.com>
2024-01-08 14:52:01 -0500

    Set global font for navigation controller titles

--- iMirror/AppDelegate.swift
+++ iMirror/AppDelegate.swift
@@ -13,6 +13,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+    
+    let navigationBarAppearace = UINavigationBar.appearance()
+    navigationBarAppearace.titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: "Roboto-Bold", size: 20)!]
+    navigationBarAppearace.largeTitleTextAttributes = [NSAttributedString.Key.font: UIFont(name: "Roboto-Bold", size: 37)!]
+    
     return true
   }
   
