4fbbebb370b1c82f55266d16b43fb5d244cadd9a
cendress <95492688+cendress@users.noreply.github.com>
2024-01-22 15:23:01 -0500

    Add question label

--- iMirror/Controllers/EmotionListVC.swift
+++ iMirror/Controllers/EmotionListVC.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class EmotionListVC: UIViewController {
   
+  //MARK: - Initial setup
+  
+  let questionLabel = ReuseableUI.createLabel(withText: "OK, what emotions are you feeling?")
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    view.backgroundColor = .systemBackground
     
     navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .close, target: self, action: #selector(closeButtonTapped))
     navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "chevron.left.circle.fill"), style: .plain, target: self, action: #selector(backButtonTapped))
+    
+    setupViews()
   }
   
   //MARK: - @objc methods
@@ -24,6 +31,12 @@ class EmotionListVC: UIViewController {
   
   @objc private func backButtonTapped() {
     navigationController?.popViewController(animated: true)
+  }
+  
+  //MARK: - Configuration methods
+  
+  private func setupViews() {
+    view.addSubview(questionLabel)
   }
   
 }
