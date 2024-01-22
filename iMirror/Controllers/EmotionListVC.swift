67e1b2d2e175eaf6512a55233a9e245fd4b7feae
cendress <95492688+cendress@users.noreply.github.com>
2024-01-22 14:47:58 -0500

    Add EmotionListVC

---
+++ iMirror/Controllers/EmotionListVC.swift
@@ -0,0 +1,17 @@
+//
+//  EmotionListVC.swift
+//  iMirror
+//
+//  Created by Christopher Endress on 1/22/24.
+//
+
+import UIKit
+
+class EmotionListVC: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+  
+}
