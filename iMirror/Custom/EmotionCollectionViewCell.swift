607de7b764ead8a73d498c222e4b1574cdedb2e5
cendress <95492688+cendress@users.noreply.github.com>
2024-01-23 15:02:23 -0500

    Setup constraints

--- iMirror/Custom/EmotionCollectionViewCell.swift
+++ iMirror/Custom/EmotionCollectionViewCell.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class EmotionCollectionViewCell: UICollectionViewCell {
-    
+  
   private let emotionImageView = UIImageView()
   private let emotionLabel = UILabel()
   
@@ -22,6 +22,27 @@ class EmotionCollectionViewCell: UICollectionViewCell {
   }
   
   private func configureCell() {
+    // Configure image view
+    emotionImageView.contentMode = .scaleAspectFit
+    emotionImageView.translatesAutoresizingMaskIntoConstraints = false
+    contentView.addSubview(emotionImageView)
     
+    // Configure label
+    emotionLabel.textAlignment = .center
+    emotionLabel.translatesAutoresizingMaskIntoConstraints = false
+    contentView.addSubview(emotionLabel)
+    
+    // Set up constraints
+    NSLayoutConstraint.activate([
+      emotionImageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+      emotionImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor, constant: -10),
+      emotionImageView.widthAnchor.constraint(equalToConstant: ReuseableUI.padding), 
+      emotionImageView.heightAnchor.constraint(equalToConstant: ReuseableUI.padding),
+      
+      emotionLabel.topAnchor.constraint(equalTo: emotionImageView.bottomAnchor, constant: 5),
+      emotionLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+      emotionLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+      emotionLabel.bottomAnchor.constraint(lessThanOrEqualTo: contentView.bottomAnchor)
+    ])
   }
 }
