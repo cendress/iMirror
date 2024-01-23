6f50988c9d2ba6394b6a8719a0d132985f2ca5e7
cendress <95492688+cendress@users.noreply.github.com>
2024-01-23 14:58:57 -0500

    Change label to image view

--- iMirror/Custom/EmotionCollectionViewCell.swift
+++ iMirror/Custom/EmotionCollectionViewCell.swift
@@ -9,14 +9,19 @@ import UIKit
 
 class EmotionCollectionViewCell: UICollectionViewCell {
     
-  private let emotionImage = UILabel()
-  private let emotionText = UILabel()
+  private let emotionImageView = UIImageView()
+  private let emotionLabel = UILabel()
   
   override init(frame: CGRect) {
     super.init(frame: frame)
+    configureCell()
   }
   
   required init(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented.")
+  }
+  
+  private func configureCell() {
+    
   }
 }
