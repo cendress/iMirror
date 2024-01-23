2b028354baaaad4cc33a74110a3e97bd4105f2e9
cendress <95492688+cendress@users.noreply.github.com>
2024-01-23 14:49:48 -0500

    Add custom collection view cell class

---
+++ iMirror/Custom/EmotionCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  EmotionCollectionViewCell.swift
+//  iMirror
+//
+//  Created by Christopher Endress on 1/23/24.
+//
+
+import UIKit
+
+class EmotionCollectionViewCell: UICollectionViewCell {
+    
+}
