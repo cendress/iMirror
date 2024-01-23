e03daebb05bcba351fa3022c4a6c86ad726f7a5f
cendress <95492688+cendress@users.noreply.github.com>
2024-01-23 15:36:03 -0500

    Customize cell appearance

--- iMirror/Custom/EmotionCollectionViewCell.swift
+++ iMirror/Custom/EmotionCollectionViewCell.swift
@@ -23,16 +23,31 @@ class EmotionCollectionViewCell: UICollectionViewCell {
   }
   
   private func configureCell() {
+    contentView.layer.cornerRadius = 15
+    contentView.backgroundColor = .systemBackground
+    contentView.layer.borderWidth = 1.0
+    contentView.layer.borderColor = UIColor.lightGray.withAlphaComponent(0.5).cgColor
+    
+    layer.shadowColor = UIColor.black.cgColor
+    layer.shadowOffset = CGSize(width: 0, height: 4)
+    layer.shadowRadius = 6
+    layer.shadowOpacity = 0.15
+    layer.masksToBounds = false
+    
     // Configure image view
     emotionImageView.contentMode = .scaleAspectFit
+    emotionImageView.layer.cornerRadius = 20
+    emotionImageView.clipsToBounds = true
+    emotionImageView.backgroundColor = UIColor.systemGray6
     emotionImageView.translatesAutoresizingMaskIntoConstraints = false
     contentView.addSubview(emotionImageView)
     
     // Configure label
     emotionLabel.textAlignment = .center
+    emotionLabel.font = UIFont(name: "Roboto-Regular", size: 14)
+    emotionLabel.textColor = UIColor.darkGray
     emotionLabel.translatesAutoresizingMaskIntoConstraints = false
     contentView.addSubview(emotionLabel)
-    
   }
   
   private func setupConstraints() {
