9974e9992aa7d6314d349011e9027ccd1db3dc19
cendress <95492688+cendress@users.noreply.github.com>
2024-01-26 11:47:09 -0500

    Add done button to keyboard

--- iMirror/Controllers/JournalNotesVC.swift
+++ iMirror/Controllers/JournalNotesVC.swift
@@ -35,6 +35,7 @@ class JournalNotesVC: UIViewController, UITextViewDelegate {
     setupTextViews()
     setupViews()
     setupConstraints()
+    addDoneButtonOnKeyboard()
   }
   
   //MARK: - @objc methods
@@ -173,6 +174,22 @@ class JournalNotesVC: UIViewController, UITextViewDelegate {
       }
       textView.textColor = UIColor.lightGray
     }
+  }
+  
+  //MARK: - Dismiss keyboard method
+  
+  private func addDoneButtonOnKeyboard() {
+    let doneToolbar: UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+    doneToolbar.barStyle = .default
+    
+    let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+    let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(dismissKeyboard))
+    
+    doneToolbar.items = [flexSpace, done]
+    doneToolbar.sizeToFit()
+    
+    titleTextView.inputAccessoryView = doneToolbar
+    notesTextView.inputAccessoryView = doneToolbar
   }
   
   //MARK: - Deinit
