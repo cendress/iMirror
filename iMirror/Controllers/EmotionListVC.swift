029954ffb207e590dca4374ee970b8e3a439d337
cendress <95492688+cendress@users.noreply.github.com>
2024-01-27 15:36:50 -0500

    Remove delegate pattern

--- iMirror/Controllers/EmotionListVC.swift
+++ iMirror/Controllers/EmotionListVC.swift
@@ -7,10 +7,6 @@
 
 import UIKit
 
-protocol EmotionSelectionDelegate: AnyObject {
-  func didSelectEmotions(emotions: [String])
-}
-
 class EmotionListVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
   
   //MARK: - Initial setup
