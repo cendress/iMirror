4440c88f474e6c8a4f85f8d6769b8d4ea7d04f2a
cendress <95492688+cendress@users.noreply.github.com>
2024-01-28 15:30:19 -0500

    Set init methods

--- iMirror/Custom/Cells/JournalEntryCell.swift
+++ iMirror/Custom/Cells/JournalEntryCell.swift
@@ -18,5 +18,17 @@ class JournalEntryCell: UITableViewCell {
   private let timeLabel = UILabel()
   
   // Initialization
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    setupCell()
+  }
   
+  required init(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented.")
+  }
+  
+  // Configuration
+  private func setupCell() {
+    
+  }
 }
