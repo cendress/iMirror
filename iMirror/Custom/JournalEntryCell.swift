c62436915e20214c991bcb3159f993c2d4d31e6e
cendress <95492688+cendress@users.noreply.github.com>
2024-01-28 15:20:44 -0500

    Create a custom table view cell class

---
+++ iMirror/Custom/JournalEntryCell.swift
@@ -0,0 +1,23 @@
+//
+//  JournalEntryCell.swift
+//  iMirror
+//
+//  Created by Christopher Endress on 1/28/24.
+//
+
+import UIKit
+
+class JournalEntryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
