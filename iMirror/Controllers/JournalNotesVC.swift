47ba929808353465674957190e8c79afba543684
cendress <95492688+cendress@users.noreply.github.com>
2024-02-01 16:57:44 -0500

    Decrease tool bar height

--- iMirror/Controllers/JournalNotesVC.swift
+++ iMirror/Controllers/JournalNotesVC.swift
@@ -61,7 +61,7 @@ class JournalNotesVC: UIViewController, UITextViewDelegate {
       
       let keyboardTop = view.frame.height - keyboardFrame.height
       let textViewBottom = activeTextView.convert(activeTextView.bounds, to: view).maxY
-      let toolbarHeight: CGFloat = 28
+      let toolbarHeight: CGFloat = 1
       
       if textViewBottom + toolbarHeight > keyboardTop {
         view.frame.origin.y = -(textViewBottom - keyboardTop + toolbarHeight)
