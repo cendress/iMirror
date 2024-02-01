e8e82601b43cff4a315654b0f376c1203373765b
cendress <95492688+cendress@users.noreply.github.com>
2024-02-01 14:39:15 -0500

    Implement container view

--- iMirror/Custom/Cells/JournalEntryCell.swift
+++ iMirror/Custom/Cells/JournalEntryCell.swift
@@ -17,6 +17,7 @@ class JournalEntryCell: UITableViewCell {
   private let moodBackgroundView = UIView()
   private let scrollView = UIScrollView()
   private let emotionsStackView = UIStackView()
+  private let containerView = UIView()
   
   override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
     super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -24,18 +25,18 @@ class JournalEntryCell: UITableViewCell {
   }
   
   required init(coder aDecoder: NSCoder) {
-    fatalError("init(coder:) has not been implemented.")
+    fatalError("init(coder:) has not been implemented")
   }
   
   private func setupCell() {
-    contentView.addSubview(moodBackgroundView)
-    contentView.addSubview(moodLabel)
-    contentView.addSubview(titleLabel)
-    contentView.addSubview(noteLabel)
-    contentView.addSubview(dateLabel)
-    contentView.addSubview(timeLabel)
-    contentView.addSubview(scrollView)
-    
+    contentView.addSubview(containerView)
+    containerView.addSubview(moodBackgroundView)
+    containerView.addSubview(moodLabel)
+    containerView.addSubview(titleLabel)
+    containerView.addSubview(noteLabel)
+    containerView.addSubview(dateLabel)
+    containerView.addSubview(timeLabel)
+    containerView.addSubview(scrollView)
     scrollView.addSubview(emotionsStackView)
     
     emotionsStackView.axis = .horizontal
@@ -44,6 +45,7 @@ class JournalEntryCell: UITableViewCell {
     emotionsStackView.spacing = 8
     
     configureAppearance()
+    setupContainerViewConstraints()
     setupConstraints()
   }
   
@@ -57,13 +59,25 @@ class JournalEntryCell: UITableViewCell {
     moodBackgroundView.backgroundColor = UIColor.systemGray5
     moodBackgroundView.layer.cornerRadius = 20
     
-    contentView.backgroundColor = UIColor.white
-    contentView.layer.cornerRadius = 20
-    contentView.layer.shadowColor = UIColor.black.cgColor
-    contentView.layer.shadowOffset = CGSize(width: 0, height: 2)
-    contentView.layer.shadowRadius = 4
-    contentView.layer.shadowOpacity = 0.1
-    contentView.clipsToBounds = false
+    containerView.backgroundColor = UIColor.white
+    containerView.layer.borderWidth = 1
+    containerView.layer.borderColor = UIColor.black.cgColor
+    containerView.layer.cornerRadius = 20
+    containerView.layer.shadowColor = UIColor.black.cgColor
+    containerView.layer.shadowOffset = CGSize(width: 0, height: 2)
+    containerView.layer.shadowRadius = 4
+    containerView.layer.shadowOpacity = 0.1
+    containerView.clipsToBounds = true
+  }
+  
+  private func setupContainerViewConstraints() {
+    containerView.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      containerView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+      containerView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+      containerView.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.9),
+      containerView.heightAnchor.constraint(equalTo: contentView.heightAnchor, constant: -20)
+    ])
   }
   
   private func setupConstraints() {
@@ -79,37 +93,38 @@ class JournalEntryCell: UITableViewCell {
     NSLayoutConstraint.activate([
       moodBackgroundView.centerYAnchor.constraint(equalTo: moodLabel.centerYAnchor),
       moodBackgroundView.centerXAnchor.constraint(equalTo: moodLabel.centerXAnchor),
-      moodBackgroundView.widthAnchor.constraint(equalTo: moodLabel.widthAnchor, constant: ReuseableUI.smallPadding),
+      moodBackgroundView.widthAnchor.constraint(equalTo: moodLabel.widthAnchor, constant: 20),
       moodBackgroundView.heightAnchor.constraint(equalTo: moodBackgroundView.widthAnchor),
       
-      moodLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: ReuseableUI.smallPadding),
-      moodLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: ReuseableUI.smallPadding),
+      moodLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 20),
+      moodLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
       
-      titleLabel.topAnchor.constraint(equalTo: moodLabel.topAnchor),
-      titleLabel.leadingAnchor.constraint(equalTo: moodBackgroundView.trailingAnchor, constant: 10),
-      titleLabel.trailingAnchor.constraint(lessThanOrEqualTo: timeLabel.leadingAnchor, constant: -10),
+      titleLabel.topAnchor.constraint(equalTo: moodLabel.bottomAnchor, constant: 10),
+      titleLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
+      titleLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20),
       
-      timeLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: ReuseableUI.smallPadding),
-      timeLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -ReuseableUI.smallPadding),
+      noteLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10),
+      noteLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
+      noteLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20),
       
-      noteLabel.topAnchor.constraint(equalTo: moodLabel.bottomAnchor, constant: 10),
-      noteLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: ReuseableUI.smallPadding),
-      noteLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -ReuseableUI.smallPadding),
+      dateLabel.topAnchor.constraint(equalTo: noteLabel.bottomAnchor, constant: 10),
+      dateLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
+      dateLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20),
       
-      scrollView.topAnchor.constraint(equalTo: noteLabel.bottomAnchor, constant: 10),
-      scrollView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: ReuseableUI.smallPadding),
-      scrollView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -ReuseableUI.smallPadding),
-      scrollView.heightAnchor.constraint(equalToConstant: ReuseableUI.padding),
+      timeLabel.topAnchor.constraint(equalTo: dateLabel.bottomAnchor, constant: 10),
+      timeLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
+      timeLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20),
       
-      emotionsStackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
-      emotionsStackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
-      emotionsStackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
-      emotionsStackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
-      emotionsStackView.heightAnchor.constraint(equalTo: scrollView.heightAnchor),
+      scrollView.topAnchor.constraint(equalTo: timeLabel.bottomAnchor, constant: 10),
+      scrollView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
+      scrollView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20),
+      scrollView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -20),
       
-      dateLabel.topAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: 10),
-      dateLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: ReuseableUI.smallPadding),
-      dateLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -ReuseableUI.smallPadding)
+      emotionsStackView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor),
+      emotionsStackView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor),
+      emotionsStackView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor),
+      emotionsStackView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor),
+      emotionsStackView.heightAnchor.constraint(equalTo: scrollView.heightAnchor)
     ])
   }
   
@@ -124,8 +139,7 @@ class JournalEntryCell: UITableViewCell {
   
   private func createEmotionLabels(for emotions: [String]) {
     emotionsStackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
-    
-    for emotion in emotions {
+    emotions.forEach { emotion in
       let label = UILabel()
       label.text = emotion
       label.backgroundColor = UIColor.systemGray6
