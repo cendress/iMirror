a015aeba0de711fd897b007f8e8898fa298788fd
cendress <95492688+cendress@users.noreply.github.com>
2024-02-04 15:20:55 -0500

    Add computed property to JournalEntry to track when note label expands

---
+++ iMirror/Models/JournalEntry+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  JournalEntry+Extensions.swift
+//  iMirror
+//
+//  Created by Christopher Endress on 2/4/24.
+//
+
+import Foundation
+
+extension JournalEntry {
+  var isExpanded: Bool {
+    return false
+  }
+}
