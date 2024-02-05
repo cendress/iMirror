01cf3e8f5d677774131561c494dc3ef207ca3dd4
cendress <95492688+cendress@users.noreply.github.com>
2024-02-05 15:22:26 -0500

    Create JournalSection struct

---
+++ iMirror/Models/JournalSection.swift
@@ -0,0 +1,13 @@
+//
+//  JournalSection.swift
+//  iMirror
+//
+//  Created by Christopher Endress on 2/5/24.
+//
+
+import Foundation
+
+struct JournalSection {
+    let date: Date
+    var entries: [JournalEntry]
+}
