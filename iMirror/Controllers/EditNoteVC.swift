2b7d0d39756d00d35474809013e52aff2bcd433e
cendress <95492688+cendress@users.noreply.github.com>
2024-02-05 14:32:56 -0500

    Implement EditNoteVC

--- iMirror/Controllers/EditNoteVC.swift
+++ iMirror/Controllers/EditNoteVC.swift
@@ -8,22 +8,37 @@
 import UIKit
 
 class EditNoteVC: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+  var noteText: String?
+  var completion: ((String) -> Void)?
+  
+  private let textView = UITextView()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemBackground
+    setupTextView()
+  }
+  
+  private func setupTextView() {
+    view.addSubview(textView)
+    textView.translatesAutoresizingMaskIntoConstraints = false
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    NSLayoutConstraint.activate([
+      textView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+      textView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+      textView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+      textView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+    ])
+    
+    textView.text = noteText
+    textView.font = UIFont(name: "Roboto-Regular", size: 17)
+    textView.textColor = .label
+    
+    navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneEditing))
+  }
+  
+  @objc private func doneEditing() {
+    completion?(textView.text)
+    dismiss(animated: true, completion: nil)
+  }
 }
