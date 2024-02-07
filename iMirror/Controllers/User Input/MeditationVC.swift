0f2b13e26bb8aee8b0f84c85780f34fbccd36b72
cendress <95492688+cendress@users.noreply.github.com>
2024-02-07 14:37:15 -0500

    Create MeditationVC

---
+++ iMirror/Controllers/User Input/MeditationVC.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationVC.swift
+//  iMirror
+//
+//  Created by Christopher Endress on 2/7/24.
+//
+
+import UIKit
+
+class MeditationVC: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+  
+}
