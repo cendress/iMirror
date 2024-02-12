3ce310676ef874c952f0d58aaacdd3c4d59fe7dc
cendress <95492688+cendress@users.noreply.github.com>
2024-02-12 13:13:32 -0500

    Finalize selecting videos

--- iMirror/Controllers/User Input/MeditationVC.swift
+++ iMirror/Controllers/User Input/MeditationVC.swift
@@ -26,7 +26,7 @@ class MeditationVC: UIViewController {
   private var soundButton: UIBarButtonItem?
   private var changeVideoButtonItem: UIBarButtonItem?
   
-  private var videoFiles = ["waves", "tunnel", "particles", "moon", "neonTunnel"]
+  private var videoFiles = ["galaxy", "waves", "neonTunnel", "particles"]
   private var currentVideoIndex = 0
   
   private var hideNavBarTimer: Timer?
