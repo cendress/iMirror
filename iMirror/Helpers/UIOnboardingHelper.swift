c6ed942bab59675356885875ff8bcb64caa35d8d
cendress <95492688+cendress@users.noreply.github.com>
2024-02-12 14:59:51 -0500

    Configure onboarding struct

--- iMirror/Helpers/UIOnboardingHelper.swift
+++ iMirror/Helpers/UIOnboardingHelper.swift
@@ -11,7 +11,7 @@ import UIOnboarding
 struct UIOnboardingHelper {
   // App Icon
   static func setUpIcon() -> UIImage {
-    return Bundle.main.appIcon ?? .init(named: "onboarding-icon")!
+    return Bundle.main.appIcon ?? .init(named: "AppIcon")!
   }
   
   // First Title Line
@@ -23,39 +23,42 @@ struct UIOnboardingHelper {
   // Second Title Line
   // App Name
   static func setUpSecondTitleLine() -> NSMutableAttributedString {
-    .init(string: Bundle.main.displayName ?? "Insignia", attributes: [
-      .foregroundColor: UIColor.init(named: "camou")!
+    .init(string: Bundle.main.displayName ?? "iMirror", attributes: [
+      .foregroundColor: UIColor.init(named: "AppColor")!
     ])
   }
   
   // Core Features
-  static func setUpFeatures() -> Array<UIOnboardingFeature> {
-    return .init([
-      .init(icon: .init(named: "feature-1")!,
-            title: "Search until found",
-            description: "Over a hundred insignia of the Swiss Armed Forces – each redesigned from the ground up."),
-      .init(icon: .init(named: "feature-2")!,
-            title: "Enlist prepared",
-            description: "Practice with the app and pass the rank test on the first run."),
-      .init(icon: .init(named: "feature-3")!,
-            title: "#teamarmee",
-            description: "Add name tags of your comrades or cadre. Insignia automatically keeps every name tag you create in iCloud.")
-    ])
+  static func setUpFeatures() -> [UIOnboardingFeature] {
+    return [
+      .init(icon: UIImage(systemName: "person.text.rectangle")!,
+            title: "Emotional Check-In",
+            description: "Begin your journey with an intuitive emotional check-in, guiding you towards understanding your current state of mind."),
+      .init(icon: UIImage(systemName: "headphones.circle")!,
+            title: "Meditation",
+            description: "Experience tools that facilitate mindfulness, aiding in processing and greater insight."),
+      .init(icon: UIImage(systemName: "note.text")!,
+            title: "Reflective Journaling",
+            description: "Culminate your session with insightful journaling, capturing your thoughts and growth post-meditation."),
+      .init(icon: UIImage(systemName: "quote.bubble")!,
+            title: "Daily Inspiration",
+            description: "Receive motivational quotes daily to foster a positive mindset and consistent mindfulness practice.")
+    ]
   }
   
   // Notice Text
   static func setUpNotice() -> UIOnboardingTextViewConfiguration {
-    return .init(icon: .init(named: "onboarding-notice-icon")!,
-                 text: "Developed and designed for members of the Swiss Armed Forces.",
-                 linkTitle: "Learn more...",
-                 link: "https://www.lukmanascic.ch/portfolio/insignia",
-                 tint: .init(named: "camou"))
+    return .init(icon: UIImage(systemName: "info.circle")!,
+                 text: "iMirror is committed to supporting your journey towards emotional well-being, respecting your privacy every step of the way.",
+                 linkTitle: "Discover more about iMirror",
+                 link: "https://www.example.com/about",
+                 tint: UIColor(named: "AppColor"))
   }
   
   // Continuation Title
   static func setUpButton() -> UIOnboardingButtonConfiguration {
     return .init(title: "Continue",
                  titleColor: .white, // Optional, `.white` by default
-                 backgroundColor: .init(named: "camou")!)
+                 backgroundColor: .init(named: "AppColor")!)
   }
 }
