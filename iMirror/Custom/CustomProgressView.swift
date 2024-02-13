0635f880c05d2815b7187e0aa92e3b0e33fbc1ae
cendress <95492688+cendress@users.noreply.github.com>
2024-02-13 13:53:13 -0500

    Remove animation UIView.animate

--- iMirror/Custom/CustomProgressView.swift
+++ iMirror/Custom/CustomProgressView.swift
@@ -68,11 +68,9 @@ class CustomProgressView: UIView {
   }
   
   private func updateSliderPosition() {
-    UIView.animate(withDuration: 0.1) {
-      let sliderPosition = self.bounds.width * self.progress
-      self.progressLayer.frame = CGRect(x: 0, y: 0, width: sliderPosition, height: self.bounds.height)
-      self.sliderKnob.center = CGPoint(x: sliderPosition, y: self.bounds.height / 2)
-    }
+    let sliderPosition = self.bounds.width * self.progress
+    self.progressLayer.frame = CGRect(x: 0, y: 0, width: sliderPosition, height: self.bounds.height)
+    self.sliderKnob.center = CGPoint(x: sliderPosition, y: self.bounds.height / 2)
   }
   
   @objc private func handlePanGesture(_ gesture: UIPanGestureRecognizer) {
