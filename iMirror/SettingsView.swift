67e2beebd089a9d40b31b725b28287f764ac4d05
cendress <95492688+cendress@users.noreply.github.com>
2024-02-19 16:10:22 -0500

    Add modifier that implements dark mode property

--- iMirror/SettingsView.swift
+++ iMirror/SettingsView.swift
@@ -42,6 +42,7 @@ struct SettingsView: View {
       }
       .navigationTitle("Settings")
       .navigationBarTitleDisplayMode(.large)
+      .preferredColorScheme(isDarkModeEnabled ? .dark : .light)
     }
   }
   
