49d6851b3787b43a65654c802655a0777278876f
cendress <95492688+cendress@users.noreply.github.com>
2024-02-21 16:29:27 -0500

    Edit acknowledgments

--- iMirror/AcknowledgmentsView.swift
+++ iMirror/AcknowledgmentsView.swift
@@ -12,26 +12,56 @@ struct AcknowledgmentsView: View {
   
   var body: some View {
     ZStack {
-      
       Color(UIColor(named: "FormBackgroundColor") ?? UIColor.systemBackground)
         .edgesIgnoringSafeArea(.all)
       
-      Text("Acknowledgments content here")
-        .toolbar {
-          ToolbarItem(placement: .navigationBarLeading) {
-            Button(action: {
-              self.presentationMode.wrappedValue.dismiss()
-            }) {
-              Image(systemName: "chevron.left.circle.fill")
-                .foregroundColor(Color(UIColor(named: "AppColor") ?? UIColor.systemBlue))
-            }
-          }
+      VStack(alignment: .center, spacing: 20) {
+        Text("Acknowledgments")
+          .font(.custom("Roboto-Bold", size: 22))
+          .padding(.bottom, 10)
+          .frame(maxWidth: .infinity, alignment: .center)
+        
+        Text("Credit to the iOS developer community, whose creativity and hardwork continue to inspire my every day.")
+          .font(.custom("Roboto-Regular", size: 18))
+          .multilineTextAlignment(.center)
+          .frame(maxWidth: .infinity, alignment: .center)
+        
+        Text("Special thanks to the creators of the UIOnboarding library, which greatly enhanced this project. Visit their GitHub repository: https://github.com/lascic/UIOnboarding.")
+          .font(.custom("Roboto-Regular", size: 18))
+          .multilineTextAlignment(.center)
+          .frame(maxWidth: .infinity, alignment: .center)
+        
+        Text("I also wish to extend my appreciation to Pixabay for providing financially accessible, high-quality videos and audio used in this app.")
+          .font(.custom("Roboto-Regular", size: 18))
+          .multilineTextAlignment(.center)
+          .frame(maxWidth: .infinity, alignment: .center)
+        
+        HStack {
+          Text("Connect with me: ")
+          Link("@chrisendress", destination: URL(string: "https://twitter.com/chrisendress_io")!)
         }
-        .navigationBarBackButtonHidden(true)
+        .font(.custom("Roboto-Regular", size: 18))
+        
+        Spacer()
+      }
+      .padding()
     }
+    
+    .toolbar {
+      ToolbarItem(placement: .navigationBarLeading) {
+        Button(action: {
+          self.presentationMode.wrappedValue.dismiss()
+        }) {
+          Image(systemName: "chevron.left.circle.fill")
+            .foregroundColor(Color(UIColor(named: "AppColor") ?? UIColor.systemBlue))
+        }
+      }
+    }
+    .navigationBarBackButtonHidden(true)
   }
 }
 
 #Preview {
   AcknowledgmentsView()
 }
+
