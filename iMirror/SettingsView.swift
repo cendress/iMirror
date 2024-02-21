231e716ffe4b45f74f0d45ffa9680471edc03c7d
cendress <95492688+cendress@users.noreply.github.com>
2024-02-21 15:02:20 -0500

    Add privacy policy

--- iMirror/SettingsView.swift
+++ iMirror/SettingsView.swift
@@ -83,7 +83,7 @@ struct SettingsView: View {
   private var supportSection: some View {
     Section(header: Text("Legal").background(Color.clear)) {
       CustomLinkView(title: "Support", url: "https://sites.google.com/view/imirrorapp/home", imageName: "person.2.fill", backgroundColor: .purple)
-      CustomLinkView(title: "Privacy Policy", url: "https://google.com", imageName: "lock.fill", backgroundColor: .green)
+      CustomLinkView(title: "Privacy Policy", url: "https://sites.google.com/view/imirrorprivacypolicy/home", imageName: "lock.fill", backgroundColor: .green)
     }
   }
   
