b9b2038a17997101d918f47c91503073f6c6ef92
cendress <95492688+cendress@users.noreply.github.com>
2024-02-21 16:32:04 -0500

    Remove title

--- iMirror/AcknowledgmentsView.swift
+++ iMirror/AcknowledgmentsView.swift
@@ -16,35 +16,27 @@ struct AcknowledgmentsView: View {
         .edgesIgnoringSafeArea(.all)
       
       VStack(alignment: .center, spacing: 20) {
-        Text("Acknowledgments")
-          .font(.custom("Roboto-Bold", size: 22))
-          .padding(.bottom, 10)
-          .frame(maxWidth: .infinity, alignment: .center)
-        
         Text("Credit to the iOS developer community, whose creativity and hardwork continue to inspire my every day.")
-          .font(.custom("Roboto-Regular", size: 18))
           .multilineTextAlignment(.center)
           .frame(maxWidth: .infinity, alignment: .center)
         
         Text("Special thanks to the creators of the UIOnboarding library, which greatly enhanced this project. Visit their GitHub repository: https://github.com/lascic/UIOnboarding.")
-          .font(.custom("Roboto-Regular", size: 18))
           .multilineTextAlignment(.center)
           .frame(maxWidth: .infinity, alignment: .center)
         
         Text("I also wish to extend my appreciation to Pixabay for providing financially accessible, high-quality videos and audio used in this app.")
-          .font(.custom("Roboto-Regular", size: 18))
           .multilineTextAlignment(.center)
           .frame(maxWidth: .infinity, alignment: .center)
         
         HStack {
-          Text("Connect with me: ")
-          Link("@chrisendress", destination: URL(string: "https://twitter.com/chrisendress_io")!)
+          Text("Connect with me:")
+          Link("@chrisendress_io", destination: URL(string: "https://twitter.com/chrisendress_io")!)
         }
-        .font(.custom("Roboto-Regular", size: 18))
         
         Spacer()
       }
       .padding()
+      .font(.custom("Roboto-Regular", size: 18))
     }
     
     .toolbar {
