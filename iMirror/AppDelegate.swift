bfa45c5856a14f892f41edcb86e6bb7687e3e987
cendress <95492688+cendress@users.noreply.github.com>
2025-06-16 19:50:43 -0400

    Prepare haptics at app launch

--- iMirror/AppDelegate.swift
+++ iMirror/AppDelegate.swift
@@ -10,107 +10,107 @@ import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-  
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
     
-    setupNavigationBarAppearance()
-    
-    UNUserNotificationCenter.current().delegate = self
-    
-    return true
-  }
-  
-  private func setupNavigationBarAppearance() {
-    DispatchQueue.global(qos: .userInitiated).async {
-      if let robotoBoldFontSmall = UIFont(name: "Roboto-Bold", size: 20),
-         let robotoBoldFontLarge = UIFont(name: "Roboto-Bold", size: 37) {
-        DispatchQueue.main.async {
-          let navigationBarAppearace = UINavigationBar.appearance()
-          navigationBarAppearace.titleTextAttributes = [NSAttributedString.Key.font: robotoBoldFontSmall]
-          navigationBarAppearace.largeTitleTextAttributes = [NSAttributedString.Key.font: robotoBoldFontLarge]
-        }
-      }
-    }
-  }
-  
-  // MARK: UISceneSession Lifecycle
-  
-  func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-    // Called when a new scene session is being created.
-    // Use this method to select a configuration to create the new scene with.
-    return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-  }
-  
-  func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-    // Called when the user discards a scene session.
-    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-  }
-  
-  // MARK: - Core Data stack
-  
-  lazy var persistentContainer: NSPersistentContainer = {
-    /*
-     The persistent container for the application. This implementation
-     creates and returns a container, having loaded the store for the
-     application to it. This property is optional since there are legitimate
-     error conditions that could cause the creation of the store to fail.
-     */
-    let container = NSPersistentContainer(name: "iMirror")
-    container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-      if let error = error as NSError? {
-        // Replace this implementation with code to handle the error appropriately.
-        // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        Haptic.prepare()
+        setupNavigationBarAppearance()
         
-        /*
-         Typical reasons for an error here include:
-         * The parent directory does not exist, cannot be created, or disallows writing.
-         * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-         * The device is out of space.
-         * The store could not be migrated to the current model version.
-         Check the error message to determine what the actual problem was.
-         */
-        fatalError("Unresolved error \(error), \(error.userInfo)")
-      }
-    })
-    return container
-  }()
-  
-  // MARK: - Core Data Saving support
-  
-  func saveContext () {
-    let context = persistentContainer.viewContext
-    if context.hasChanges {
-      do {
-        try context.save()
-      } catch {
-        // Replace this implementation with code to handle the error appropriately.
-        // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-        let nserror = error as NSError
-        fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-      }
+        UNUserNotificationCenter.current().delegate = self
+        
+        return true
     }
-  }
+    
+    private func setupNavigationBarAppearance() {
+        DispatchQueue.global(qos: .userInitiated).async {
+            if let robotoBoldFontSmall = UIFont(name: "Roboto-Bold", size: 20),
+               let robotoBoldFontLarge = UIFont(name: "Roboto-Bold", size: 37) {
+                DispatchQueue.main.async {
+                    let navigationBarAppearace = UINavigationBar.appearance()
+                    navigationBarAppearace.titleTextAttributes = [NSAttributedString.Key.font: robotoBoldFontSmall]
+                    navigationBarAppearace.largeTitleTextAttributes = [NSAttributedString.Key.font: robotoBoldFontLarge]
+                }
+            }
+        }
+    }
+    
+    // MARK: UISceneSession Lifecycle
+    
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+    
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    // MARK: - Core Data stack
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        /*
+         The persistent container for the application. This implementation
+         creates and returns a container, having loaded the store for the
+         application to it. This property is optional since there are legitimate
+         error conditions that could cause the creation of the store to fail.
+         */
+        let container = NSPersistentContainer(name: "iMirror")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    // MARK: - Core Data Saving support
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
 }
 
 extension AppDelegate: UNUserNotificationCenterDelegate {
-  func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-    
-    DispatchQueue.main.async {
-      if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-         let sceneDelegate = windowScene.delegate as? SceneDelegate,
-         let window = sceneDelegate.window,
-         let customTabBarController = window.rootViewController as? CustomTabBarController {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
         
-        customTabBarController.selectedIndex = 0
-        
-        if let journalNavController = customTabBarController.viewControllers?[0] as? UINavigationController {
-          
-          journalNavController.popToRootViewController(animated: true)
+        DispatchQueue.main.async {
+            if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+               let sceneDelegate = windowScene.delegate as? SceneDelegate,
+               let window = sceneDelegate.window,
+               let customTabBarController = window.rootViewController as? CustomTabBarController {
+                
+                customTabBarController.selectedIndex = 0
+                
+                if let journalNavController = customTabBarController.viewControllers?[0] as? UINavigationController {
+                    
+                    journalNavController.popToRootViewController(animated: true)
+                }
+            }
         }
-      }
+        completionHandler()
     }
-    completionHandler()
-  }
 }
